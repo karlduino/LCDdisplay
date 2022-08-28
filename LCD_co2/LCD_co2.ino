89b4c727f49776ff8aa4b2bf1c87cbe4d1b0ef87
Karlduino <karlduino@gmail.com>
2022-08-28 11:31:09 -0500

    LCD_co2: custom character for subscript 2

--- LCD_co2/LCD_co2.ino
+++ LCD_co2/LCD_co2.ino
@@ -5,28 +5,47 @@
 #include <LiquidCrystal_I2C.h>
 #include <string.h>
 
+#if defined(ARDUINO) && ARDUINO >= 100
+#define printByte(args)  write(args);
+#else
+#define printByte(args)  print(args,BYTE);
+#endif
+
+uint8_t subscript[8] = {0x0, 0x0, 0x0, 0x1E, 0x03, 0x06, 0x0C, 0x1F };
+// uint8_t subscript[8] = {0x0, 0x0, 0x1E, 0x03, 0x06, 0x0C, 0x1F, 0x00};
+uint8_t heart[8] = {0x0,0xa,0x1f,0x1f,0xe,0x4,0x0};
+
+
 LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
 
 void setup()
 {
-  lcd.init();                      // initialize the lcd 
   lcd.init();
-  // Print a message to the LCD.
   lcd.backlight();
+
+  lcd.createChar(0, heart);
+  lcd.createChar(1, subscript);
+  lcd.home();
 
   char buffer[17];
   int co2 = 589;
-  sprintf(buffer, "%4d ppm CO₂", co2);
+  sprintf(buffer, "%4d ppm CO", co2);
 
   lcd.setCursor(0,0);
   lcd.print(buffer);
+
+  lcd.setCursor(11,0);
+  lcd.printByte(1);
 
   int temp = 79;
   sprintf(buffer, "%3d", temp);
   lcd.setCursor(0,1);
   lcd.print(buffer);
+  lcd.setCursor(3,1);
+  lcd.print((char)223); // degree symbol
   lcd.setCursor(4,1);
-  lcd.print("°F");
+  lcd.print("F");
 
   int humidity = 49;
   sprintf(buffer, "%3d", humidity);
@@ -34,6 +53,7 @@ void setup()
   lcd.print(buffer);
   lcd.setCursor(11,1);
   lcd.print("%");
+
 }
 
 
